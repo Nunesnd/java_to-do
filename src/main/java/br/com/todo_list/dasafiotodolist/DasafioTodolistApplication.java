172100558be2b6e2444b84d270a4617df4b06f28
Diego Nunes <nunesdiegond@gmail.com>
2024-05-11 14:46:56 -0300

    criando o desafio

---
+++ src/main/java/br/com/todo_list/dasafiotodolist/DasafioTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.todo_list.dasafiotodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasafioTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasafioTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/todo_list/dasafiotodolist/DasafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.todo_list.dasafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
