cd8fbaec65e51d0ffe57934810024a8e8ded249b
Diego Nunes <nunesdiegond@gmail.com>
2024-05-11 15:53:17 -0300

    criado controlador

---
+++ src/main/java/br/com/todo_list/dasafiotodolist/controller/TodoController.java
@@ -0,0 +1,48 @@
+package br.com.todo_list.dasafiotodolist.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.todo_list.dasafiotodolist.entidades.Todo;
+import br.com.todo_list.dasafiotodolist.servicos.TodoServico;
+
+@RestController
+@RequestMapping("/todos")
+
+public class TodoController {
+
+    private TodoServico todoServico;
+    
+    public TodoController(TodoServico todoServico) {
+        this.todoServico = todoServico;
+    }
+
+    @PostMapping
+    List<Todo> criar(@RequestBody Todo todo){
+        return todoServico.criar(todo);
+    }
+
+    @GetMapping
+    List<Todo> listar(){
+        return todoServico.listar();
+    }
+
+    @PutMapping
+    List<Todo> atualizar(@RequestBody Todo todo){
+        return todoServico.criar(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> apagar(@PathVariable("id") Long id){
+        return todoServico.apagar(id);
+    }
+
+}
